15e6c572e61214a98ccb30c5d729810b660ed224
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-09-01 22:48:03 +0400

    init

---
+++ countdown.py
@@ -0,0 +1,50 @@
+import os
+import time
+from pyfiglet import Figlet
+fig = Figlet(font='slant')
+
+
+def clear(): return os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def countdown(_type):
+    def countdown1(_func):
+        def wrapper():
+            maxNum = _func()
+            clear()
+            for i in range(maxNum, 0, -1):
+                print(fig.renderText(str(i)))
+                time.sleep(1)
+                clear()
+            print(fig.renderText('FINISH'))
+        return wrapper
+
+    def countdown2(_func):
+        def wrapper():
+            maxNum = _func()
+            clear()
+            for i in range(maxNum-1, -1, -1):
+                for j in range(9, -1, -1):
+                    print(fig.renderText(f"{i} . {j}"))
+                    time.sleep(0.1)
+                    clear()
+            print(fig.renderText('FINISH'))
+        return wrapper
+    types = ['1', '2']
+    if _type in types:
+        return locals()['countdown'+_type]
+    else:
+        def errorHandling(_func):
+            def wrapper():
+                print('Type is not valid. Valid types: '+", ".join(types))
+            return wrapper
+        return errorHandling
+
+
+@countdown(input('Enter countdown type: '))
+def num():
+    clear()
+    return int(input('Set Countdown: '))
+
+
+num()
